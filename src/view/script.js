982f8534dbf9333648dcfc4146fdc9f3e06fa80a
Felipe D. Santos <lycan.felipe@gmail.com>
2020-08-28 20:31:17 -0300

    Validação de campos vazios e validação de email invalido

--- src/view/script.js
+++ src/view/script.js
@@ -18,12 +18,36 @@ btnNovo.addEventListener('click', () => {
 //Persistindo os dados
 btnSalvar.addEventListener('click', () => {
     event.preventDefault();
+
+    //Regras de validação
+    function valida_form() {
+        var email = inputEmail.value;
+        var nome = inputNome.value;
+        var sobrenome = inputSobreNome.value;
+        var nascimento = inputNascimento.value;
+
+        if (email == '' || nome == '' || sobrenome == '' || nascimento == '')
+        {
+            alert('Atenção todos os campos devem ser preenchidos!');
+
+            return;
+        }
+
+        var atpos = email.indexOf("@");
+        var dotpos = email.lastIndexOf(".");
+
+        if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length) {
+            alert("Por favor, digite um endereço de e-mail válido");
+
+            inputEmail.focus();
+            return;
+
+        }
+        
+    //Chamando o back end
     const url = 'http://127.0.0.1:8080/novo';
 
-    console.log(inputEmail.value)
-
     const novoContato = {
-            //"id": 34,
             nome: inputNome.value,
             sobrenome: inputSobreNome.value,
             nascimento: inputNascimento.value,
@@ -33,13 +57,28 @@ btnSalvar.addEventListener('click', () => {
             enderecos: []
     }
 
-    console.log(novoContato.email);
-
     axios.post(url, novoContato)
         .then((response) => {
             console.log(response.data);
         })
         .catch((error) => {
            console.log({ "Message: ": "Desulpe algo deu errado" + error })
-        })
+        });
+
+    inputNome.value = ''
+    inputSobreNome.value = ''
+    inputNascimento.value = ''
+    inputEmail.value = ''
+
+    inputNascimento.disabled = true;
+    inputNome.disabled = true;
+    inputSobreNome.disabled = true;
+    inputEmail.disabled = true;
+    btnSalvar.disabled = true;
+
+    }
+    valida_form();
+
+
+    
 })
