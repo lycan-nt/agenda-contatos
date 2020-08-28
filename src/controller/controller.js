5be701235190a287cc623db660b2aaf8cfeb5c72
Felipe D. Santos <lycan.felipe@gmail.com>
2020-08-28 17:53:23 -0300

    Teste-de-post

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -16,14 +16,12 @@ exports.inserir = (req, res) => {
     const url = 'https://agendjango.herokuapp.com/api/pessoas/'
 
     const novoContato = {
-        nome: "Foto-Teste",
-        sobrenome: "SN",
-        nascimento: "2020-07-14",
-        email: "teste@teste.com.br",
-        foto: "C:\Users\SYSALA1\Pictures\Saved Pictures\1.jpeg",
-        telefones: [
-            
-        ],
+        nome: req.body.nome,
+        sobrenome: req.body.sobrenome,
+        nascimento: req.body.nascimento,
+        email: req.body.email,
+        foto: "",
+        telefones: [],
         enderecos: []
     }
 
--- src/view/script.js
+++ src/view/script.js
@@ -2,15 +2,43 @@
 const btnNovo = document.querySelector(".novo");
 const btnSalvar = document.querySelector(".salvar");
 
-const inputNacimento = document.querySelector('#data');
+const inputNascimento = document.querySelector('#data');
 const inputNome = document.querySelector('.input-nome');
 const inputSobreNome = document.querySelector('.input-sobrenome');
 const inputEmail = document.querySelector('.input-email');
 
 btnNovo.addEventListener('click', () => {
-    inputNacimento.disabled = false;
+    inputNascimento.disabled = false;
     inputNome.disabled = false;
     inputSobreNome.disabled = false;
     inputEmail.disabled = false;
     btnSalvar.disabled = false;
+})
+
+//Persistindo os dados
+btnSalvar.addEventListener('click', () => {
+    const url = 'http://127.0.0.1:8080/novo';
+
+    console.log(inputEmail.value)
+
+    const novoContato = {
+            //"id": 34,
+            nome: inputNome.value,
+            sobrenome: inputSobreNome.value,
+            nascimento: inputNascimento.value,
+            email: inputEmail.value,
+            foto: "",
+            telefones: [],
+            enderecos: []
+    }
+
+    console.log(novoContato.email);
+
+    axios.post(url, novoContato)
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+           console.log({ "Message: ": "Desulpe algo deu errado" + error })
+        })
 })
