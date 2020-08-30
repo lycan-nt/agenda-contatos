79a299c14d6e735ae8e8f8ba6132bf7311a33e5b
Felipe D. Santos <lycan.felipe@gmail.com>
2020-08-30 02:26:29 -0300

    Implementando o campo buscar para a tabela de listagem

--- src/view/scripts/tabela.js
+++ src/view/scripts/tabela.js
@@ -14,7 +14,7 @@
                             "<td>" + data[i].id + "</td>" +
                             "<td>" + data[i].nome + "</td>" +
                             "<td>" + data[i].sobrenome + "</td>" +
-                            "<td>" + data[i].nascimento + "</td>" +
+                            "<td>" + data[i].nascimento.substr(0, 10).split('-').reverse().join('/') + "</td>" +
                             "<td>" + data[i].email + "</td>" +
                             "</tr>"
                             tBody.innerHTML += tr;
@@ -33,7 +33,37 @@
     const dados = recuperaDados();
 
     
+document.querySelector(".buscar").addEventListener('keyup', () => {
+   var coluna = "1";
+   var filtrar, tabela, tr, td, th, i;
 
+   filtrar = document.querySelector(".buscar");
+   filtrar = filtrar.value.toUpperCase();
+
+   tabela = document.querySelector(".table");
+
+   tr = tabela.getElementsByTagName("tr");
+
+   th = tabela.getElementsByTagName("th");
+
+   for (i = 0; i < tr.length; i++)
+   {
+       td = tr[i].getElementsByTagName("td")[coluna];
+
+       if (td) 
+       {
+           if (td.innerHTML.toUpperCase().indexOf(filtrar) > -1)
+           {
+               tr[i].style.display = '';
+           } 
+           else
+           {
+            tr[i].style.display = 'none';
+           }
+       }
+
+   }
+});
 
 
 
